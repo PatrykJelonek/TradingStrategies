c2053b15c62d69ea90b08f234ff5cb0406f5fc66
Patryk <ghostzoombi@gmail.com>
2022-08-16 14:47:23 +0200

    Update StuckeyStrategy.php

--- src/Strategies/Stuckey/StuckeyStrategy.php
+++ src/Strategies/Stuckey/StuckeyStrategy.php
@@ -4,5 +4,29 @@ namespace TradingStrategies\Strategies\Stuckey;
 
 class StuckeyStrategy
 {
-  // @todo Implement Stuckey trading strategy
+  private const DEFAULT_FACTOR = 0.5;
+  private const DEFAULT_ITERATION_OFFSET = 3900;
+  private const DEFAULT_SPREAD = 1;
+  
+  private array $data = [];
+  private int $dataSize = 0;
+  private float $factor = self::DEFAULT_FACTOR;
+  private float $iterationOffset = self::DEFAULT_ITERATION_OFFSET;
+  private float $spread = self::DEFAULT_SPREAD;
+  
+  /**
+  * @param Item[] $data
+  */
+  public function __construct(
+    array $data, 
+    float $factor = self::DEFAULT_FACTOR, 
+    float $iterationOffset = self::DEFAULT_ITERATION_OFFSET, 
+    float $spread = self::DEFAULT_SPREAD
+  ) {
+    $this->data = $data;
+    $this->factor = $factor;
+    $this->iterationOffset = $iterationOffset;
+    $this->spread = $spread;
+    $this->dataSize = count($data);
+  }
 }
