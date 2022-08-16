e23d15a30546eaf047b75834d263ba2954c96ee3
Patryk <ghostzoombi@gmail.com>
2022-08-16 12:15:55 +0200

    Create OHLC.php

---
+++ src/Structures/OHLC.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TradingStrategies\Structures;
+
+class OHLC
+{
+ private float $open = 0.0;
+  private float $high = 0.0;
+  private float $low = 0.0;
+  private flaot $close = 0.0.;
+}
