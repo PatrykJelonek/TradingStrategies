bcb6991fdca48f96c7390dd13ec18faa1db416d6
Patryk Jelonek <patryk.jelonek@furgonetka.pl>
2022-08-18 19:06:24 +0200

    Project initialization

--- src/Strategies/Stuckey/StuckeyStrategy.php
+++ src/Strategies/Stuckey/StuckeyStrategy.php
@@ -2,31 +2,33 @@
 
 namespace TradingStrategies\Strategies\Stuckey;
 
+use TradingStrategies\Structures\Item;
+
 class StuckeyStrategy
 {
-  private const DEFAULT_FACTOR = 0.5;
-  private const DEFAULT_ITERATION_OFFSET = 3900;
-  private const DEFAULT_SPREAD = 1;
-  
-  private array $data = [];
-  private int $dataSize = 0;
-  private float $factor = self::DEFAULT_FACTOR;
-  private float $iterationOffset = self::DEFAULT_ITERATION_OFFSET;
-  private float $spread = self::DEFAULT_SPREAD;
-  
-  /**
-  * @param Item[] $data
-  */
-  public function __construct(
-    array $data, 
-    float $factor = self::DEFAULT_FACTOR, 
-    float $iterationOffset = self::DEFAULT_ITERATION_OFFSET, 
-    float $spread = self::DEFAULT_SPREAD
-  ) {
-    $this->data = $data;
-    $this->factor = $factor;
-    $this->iterationOffset = $iterationOffset;
-    $this->spread = $spread;
-    $this->dataSize = count($data);
-  }
+    private const DEFAULT_FACTOR = 0.5;
+    private const DEFAULT_ITERATION_OFFSET = 3900;
+    private const DEFAULT_SPREAD = 1;
+
+    private array $data = [];
+    private int $dataSize = 0;
+    private float $factor = self::DEFAULT_FACTOR;
+    private float $iterationOffset = self::DEFAULT_ITERATION_OFFSET;
+    private float $spread = self::DEFAULT_SPREAD;
+
+    /**
+     * @param Item[] $data
+     */
+    public function __construct(
+        array $data,
+        float $factor = self::DEFAULT_FACTOR,
+        float $iterationOffset = self::DEFAULT_ITERATION_OFFSET,
+        float $spread = self::DEFAULT_SPREAD
+    ) {
+        $this->data = $data;
+        $this->factor = $factor;
+        $this->iterationOffset = $iterationOffset;
+        $this->spread = $spread;
+        $this->dataSize = count($data);
+    }
 }
--- src/Structures/Item.php
+++ src/Structures/Item.php
@@ -4,8 +4,8 @@ namespace TradingStrategies\Structures;
 
 class Item
 {
-  private ?string $date = null;
-  private ?OHLC $ohlc = null;
-  private float $volumen = 0.0;
-  private float $lop = 0.0;
+    private ?string $date = null;
+    private ?OHLC $ohlc = null;
+    private float $volumen = 0.0;
+    private float $lop = 0.0;
 }
